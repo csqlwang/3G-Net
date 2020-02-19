23c6cd9c112cc7e5ae9cdb926d391fb2f6c1bc8d
qlwang <38026763+csqlwang@users.noreply.github.com>
2020-02-19 20:49:24 +0800

    Update val.sh

--- scripts/Places365/val.sh
+++ scripts/Places365/val.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 now=$(date +"%Y%m%d_%H%M%S")
 
-jobname=Places365-ResNet101-Test
+jobname=3G-ResNet18-Test-Places365
 
 log_dir=logs/${jobname}
 
@@ -11,10 +11,12 @@ if [ ! -d $log_dir ]; then
 fi
 
 python -u main-10-Crop.py \
-       -a resnet101_365 \
+       -a 3g_resnet18_365 \
        -e \
        -b 64 \
        -j 16 \
+       --lr 0.1 \
+       --wd 1e-4 \
        --epochs 115 \
        -p 100 \
        --resume model_best.pth.tar \
