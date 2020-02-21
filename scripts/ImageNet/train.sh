0448b7a902022705e1a9b31d4b873e5d2ff54911
qlwang <38026763+csqlwang@users.noreply.github.com>
2020-02-21 16:38:52 +0800

    Update train.sh

--- scripts/ImageNet/train.sh
+++ scripts/ImageNet/train.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+now=$(date +"%Y%m%d_%H%M%S")
 python -u main.py \
        -a resnet50_3g \
        -b 256 \
@@ -9,4 +9,4 @@ python -u main.py \
        --epochs 115 \
        -p 100 \
        --resume checkpoint.pth.tar \
-       /home/sdc1/dataset/ILSVRC2012/images | tee ./resnet50_3g.log \ 
+       /home/sdc1/dataset/ILSVRC2012/images | tee ./resnet50-3g-${now}.log \ 
