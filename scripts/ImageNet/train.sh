280a149a9d971697e5f42d3d93f9f0ea56ed6f7a
qlwang <38026763+csqlwang@users.noreply.github.com>
2020-02-21 16:40:46 +0800

    Update train.sh

--- scripts/ImageNet/train.sh
+++ scripts/ImageNet/train.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 now=$(date +"%Y%m%d_%H%M%S")
+
+jobname=ResNet50-3G-ImageNet
+
+log_dir=logs/${jobname}
+
+if [ ! -d $log_dir ]; then
+  echo create log $log_dir
+  mkdir -p $log_dir
+fi
+
 python -u main.py \
        -a resnet50_3g \
-       -b 256 \
+       -b 64 \
        -j 16 \
        --lr 0.1 \
        --wd 1e-4 \
        --epochs 115 \
        -p 100 \
        --resume checkpoint.pth.tar \
-       /home/sdc1/dataset/ILSVRC2012/images | tee ./resnet50-3g-${now}.log \ 
+       /home/sdc1/dataset/ILSVRC2012/images | tee ./logs/${jobname}/record-train-${now}.txt \
+
