6aca33b68538da5db3875de09a4530824d8c54bb
qlwang <38026763+csqlwang@users.noreply.github.com>
2020-02-21 16:41:44 +0800

    Update val.sh

--- scripts/ImageNet/val.sh
+++ scripts/ImageNet/val.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 now=$(date +"%Y%m%d_%H%M%S")
+
+jobname=ResNet101-3G-Test-ImageNet
+
+log_dir=logs/${jobname}
+
+if [ ! -d $log_dir ]; then
+  echo create log $log_dir
+  mkdir -p $log_dir
+fi
+
 python -u main.py \
-       -a resnet50_3g \
+       -a resnet101_3g \
        -e \
        -b 256 \
        -j 16 \
@@ -9,6 +19,7 @@ python -u main.py \
        --wd 1e-4 \
        --epochs 115 \
        -p 100 \
-       --resume ResNet50-3G-ImageNet.pth.tar \
-       /home/sdc1/dataset/ILSVRC2012/images | tee ./resnet50-3g-test-${now}.txt \
+       --resume ResNet101-3G-ImageNet.pth.tar \
+       /home/sdc1/dataset/ILSVRC2012/images | tee ./logs/${jobname}/record-train-${now}.txt \
+
 
